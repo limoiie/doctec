241f49957df6c3a317822f51ba7023d3c4c14e7f
limoiie <limo.iie4@gmail.com>
2025-01-28 10:13:37 +0800

    refactor: Add interface for exposed py function in use-eel.tsx

---
+++ src/hooks/use-eel.ts
@@ -0,0 +1,34 @@
+import { eel } from "@/eel";
+import { EmbDetectionResultDataWithoutRun } from "@/types/EmbDetectionResultDataWithoutRun.schema";
+import { EmbDetectionRunData } from "@/types/EmbDetectionRunData.schema";
+
+interface Eel {
+  exit(): void;
+
+  set_host(wsUrl: string): void;
+
+  /// Following are the functions that are exposed to the frontend, see index.py
+
+  fetchEmbeddingDetectionRuns(page_no: number, page_size: number): () => Promise<EmbDetectionRunData[]>;
+
+  fetchEmbeddingDetectionRunByUuid(run_uuid: string): () => Promise<EmbDetectionRunData>;
+
+  fetchEmbeddingDetectionResultByRunUuid(run_id: string): () => Promise<EmbDetectionResultDataWithoutRun>;
+
+  detectEmbeddedFiles(cfg: any): () => Promise<string>;
+
+  deleteRun(run_uuid: string): () => Promise<boolean>;
+
+  debug(msg: string): () => Promise<void>;
+}
+
+export function useEel() {
+  function setupHost(host: string, port: number) {
+    eel.set_host(`ws://${host}:${port}`);
+  }
+
+  return {
+    eel: eel as Eel,
+    setupHost
+  };
+}
